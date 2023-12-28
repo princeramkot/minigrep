5595ec0a13394e8cdf022bba2e64291cbab620b8
prince <prince@biz.com>
2023-12-28 21:26:48 +0530

    clean using iter

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+       
+        
+        let query = match args.next() {
+            Some(s) => s,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next(){
+            Some(f) => f,
+            None => return Err("Didn't get a file path"),
+        };
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
         let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config { query, file_path, ignore_case })
     }
@@ -66,13 +72,7 @@ Trust me.";
 }
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in content.lines(){
-        if line.contains(query){
-            results.push(line);
-        }
-    }
-    return results;
+    content.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
